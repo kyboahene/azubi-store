7b41ae50eee10f165eea51a9c0ea09bbdc8251ec
kyboahene <kyeiyaw437@gmail.com>
2025-06-24 04:46:19 +0000

    feat: update Input component styles

--- modules/shared/input/index.tsx
+++ modules/shared/input/index.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-10 w-full rounded-md border border-gray-200 px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
+          "flex w-full px-4 py-3 text-sm ring-offset-background border-[1px] border-gray-300 rounded-lg focus:ring-[1px] focus:ring-[#D87D4A] focus:border-[#D87D4A] outline-none transition-colors duration-200 placeholder:text-gray-400 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
         ref={ref}
