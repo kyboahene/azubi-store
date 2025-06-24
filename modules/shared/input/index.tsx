ded3e2e53a8b7626eb339a9bc87f56d5a13208a4
kyboahene <kyeiyaw437@gmail.com>
2025-06-24 05:56:11 +0000

    fix: update input focus ring color

--- modules/shared/input/index.tsx
+++ modules/shared/input/index.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex w-full px-4 py-3 text-sm ring-offset-background border-[1px] border-gray-300 rounded-lg focus:ring-[1px] focus:ring-[#D87D4A] focus:border-[#D87D4A] outline-none transition-colors duration-200 placeholder:text-gray-400 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
+          "flex w-full px-4 py-3 text-sm ring-offset-background border-[1px] border-gray-300 rounded-lg focus:ring-[1px] focus:ring-primary-100 focus:border-primary-100 outline-none transition-colors duration-200 placeholder:text-gray-400 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
         ref={ref}
